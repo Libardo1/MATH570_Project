499eb4a20e470035436f580b2894bbae6a3cbb01
Erik Langert <eriklangert@gmail.com>
2017-04-21 15:07:28 -0400

    All good except every prediction is the word 'joke'.  I think my code is laughing at me.

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import sys
 
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from pandas import read_csv, DataFrame
 from sklearn.svm import SVC
 
@@ -24,11 +24,11 @@ training = defaultdict(list)
 testing = defaultdict(DataFrame)
 
 # This isn't working correctly. Have to change how files are read
+print "\nReading in Data ....\n"
 os.chdir('data/')
 for d in os.listdir(os.curdir):
     if d.startswith('tctodd'):
         for f in os.listdir(d):
-            print 'Reading from file:', f
             loc = f.rfind('-')
             word = f[:loc]
             num  = int(f[loc+1: loc+2])
@@ -42,25 +42,32 @@ for d in os.listdir(os.curdir):
 os.chdir('../')
 
 # Structuring Data
+print "\nStructuring Data ....\n"
 time_data = defaultdict(Data)
+test_data = defaultdict(Data)
 time = 0;
 keys = training.keys()
 while keys:
     temp_data = Data(_time=time)
+    ttest_data = Data(_time=time)
     for key in keys:
+        if time < len(testing[key]):
+            ttest_data.addData(key, testing[key].loc[[time]].as_matrix())
         if time < len(training[key][0]):
             temp_data.addData(key + '0', training[key][0].loc[[time]].as_matrix())
         if time < len(training[key][1]):
             temp_data.addData(key + '1', training[key][1].loc[[time]].as_matrix())
-        else:
+        if time >= len(training[key][0]) and time >= len(training[key][1]):
             keys.remove(key)
     time_data[time] = (temp_data)
+    test_data[time] = (ttest_data)
     time += 1
 
 
-# Classes -> Times -> Classifiers
+# Times -> Classes -> Classifiers
 SVMs = defaultdict(lambda : defaultdict(SVC))
 
+print "\nTraining Classifiers ....\n"
 # Training Classifiers
 for tim, dat in time_data.iteritems():
     signs = set([key[:-1] for key in dat.data.columns])
@@ -75,8 +82,60 @@ for tim, dat in time_data.iteritems():
         temp_cols = labels + temp_cols
         Y = [1] * len(labels) + Y
         if not all(x == Y[0] for x in Y):
-            clf = SVC()
+            clf = SVC(kernel='linear')
             clf.fit(dat.data[temp_cols].T.as_matrix(), Y)
-            SVMs[sign][tim] = clf
+            SVMs[tim][sign] = clf
 
 # Testing
+num_correct = 0
+num_total = 0
+
+print "\nTesting ....\n"
+# Iterating over test data
+for word, data in testing.iteritems():
+    sums = defaultdict(float, [(i, 0) for i in testing.keys()])
+    for i, row in data.iterrows():
+
+        # Iterating over all svms over the specified time
+        for class_, svc in SVMs[i].iteritems():
+
+            # Calculate distance from hyperplane
+            y = svc.decision_function(data.as_matrix())
+            w_norm = np.linalg.norm(svc.coef_)
+            dist = np.linalg.norm(y / w_norm) / len(data)
+
+            sums[class_] += abs(dist)
+
+    max_dist = max(sums.values())
+    clf = ''
+
+    for key, value in sums.iteritems():
+        if max_dist == value:
+            print 'max_dist', max_dist
+            clf = key
+            break
+    print word, clf
+    if word == clf:
+        num_correct += 1
+    num_total += 1
+
+'''
+# Iterating over test data
+for time_, data_ in test_data.iteritems():
+    sums = dict(zip(test_data.keys(), [0] * len(test_data)))
+
+    # Iterating over all svms over the specified time
+    for class_, svc in SVMs[time_]:
+
+        # Calculate distance from hyperplane
+        y = svc.decision_function(data_.data.as_matrix())
+        w_norm = np.linalg.norm(svc.coef_)
+        dist = y / w_norm
+
+        sums[class_] += dist
+'''
+
+
+print 'num correct', num_correct
+print 'num total', num_total
+print 'Total Accuracy', float(num_correct / num_total)
